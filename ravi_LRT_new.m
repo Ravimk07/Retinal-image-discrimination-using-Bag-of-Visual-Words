52dfa97290b8ffbf9d87524d3cdbc10c1e0e2f5f
Ravi Kamble <ravikamble07@gmail.com>
2018-09-03 21:54:12 +0530

    Add files via upload

---
+++ ravi_LRT_new.m
@@ -0,0 +1,15 @@
+function[xx,yy] = ravi_LRT_new(Y)
+
+deltaP=15; deltaN=-13;
+
+nx=5; ny=5; % (2nx+1).(2ny+1) neighborhood size
+ 
+Y=Y(:,:,2);
+
+outP=delta_LRT_extended_neighborhood(Y,deltaP,nx,ny);
+outN=delta_LRT_extended_neighborhood(Y,deltaN,nx,ny);
+outP_interval=double(LTR_component(outP,5,400));
+outN_interval=double(LTR_component(outN,5,400));
+  
+xx = uint8(imadjust(uint8(outP_interval)));
+yy = uint8(imadjust(uint8(outN_interval)));
